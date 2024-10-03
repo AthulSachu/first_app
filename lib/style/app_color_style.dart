5b458c36886e12ddf41cc4c602e7f07b3bbc5cda
AthulSachu <athulsachu80@gmail.com>
2024-10-03 20:07:18 +0530

    color style

---
+++ lib/style/app_color_style.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColorStyle {
+  static Color get primary => const Color(0xFFEBE443);
+  static Color get bgColor => const Color(0xFF151515);
+  static Color get notificationAlertColor => const Color(0xFFF01C1C);
+  static Color get backColor => const Color(0xFF1B1B1B);
+  static Color get whiteColor => const Color(0xFFF6F6F6);
+  static Color get blackColor => const Color(0xFF000000);
+}
