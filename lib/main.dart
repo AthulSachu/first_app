e064ac8d1a98dd65f45f43ef879e5d4f1cb00149
AthulSachu <athulsachu80@gmail.com>
2024-10-03 20:03:49 +0530

    main dart modified

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:first_app/style/app_color_style.dart';
 import 'package:first_app/views/ui/main_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      systemNavigationBarColor: AppColorStyle.bgColor,
+    ),
+  );
   runApp(const MyApp());
 }
 
@@ -11,6 +18,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: MainScreen());
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MainScreen(),
+    );
   }
 }
